e3ee397d5257b9311819f0c046dbffa9de8eb7c5
Zhongqi Ren <rzq23@mails.tsinghua.edu.cn>
2025-09-03 12:53:20 +0800

    完成了project1-minesweeper

--- 1.Knowledge/project1/minesweeper/minesweeper.py
+++ 1.Knowledge/project1/minesweeper/minesweeper.py
@@ -7,7 +7,6 @@ class Minesweeper():
     Minesweeper game representation
     """
 
-    # ... (The Minesweeper class is correct, no changes needed) ...
     def __init__(self, height=8, width=8, mines=8):
 
         # Set initial width, height, and number of mines
@@ -99,10 +98,9 @@ class Sentence():
     def __eq__(self, other):
         return self.cells == other.cells and self.count == other.count
 
-    # Added for use in sets to detect duplicate sentences
+     # Added for use in sets to detect duplicate sentences
     def __hash__(self):
         return hash((frozenset(self.cells), self.count))
-
     def __str__(self):
         return f"{self.cells} = {self.count}"
 
@@ -110,6 +108,8 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
+        # If the number of cells in the set is equal to the count,
+        # then all cells in the set must be mines.
         if len(self.cells) == self.count and self.count > 0:
             return self.cells.copy()
         return set()
@@ -118,6 +118,8 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be safe.
         """
+        # If the count of mines is 0, then all cells in the set
+        # must be safe.
         if self.count == 0:
             return self.cells.copy()
         return set()
@@ -146,11 +148,19 @@ class MinesweeperAI():
     """
 
     def __init__(self, height=8, width=8):
+
+        # Set initial height and width
         self.height = height
         self.width = width
+
+        # Keep track of which cells have been clicked on
         self.moves_made = set()
+
+        # Keep track of cells known to be safe or mines
         self.mines = set()
         self.safes = set()
+
+        # List of sentences about the game known to be true
         self.knowledge = []
 
     def mark_mine(self, cell):
@@ -158,102 +168,131 @@ class MinesweeperAI():
         Marks a cell as a mine, and updates all knowledge
         to mark that cell as a mine as well.
         """
-        if cell not in self.mines:
-            self.mines.add(cell)
-            for sentence in self.knowledge:
-                sentence.mark_mine(cell)
+        self.mines.add(cell)
+        for sentence in self.knowledge:
+            sentence.mark_mine(cell)
 
     def mark_safe(self, cell):
         """
         Marks a cell as safe, and updates all knowledge
         to mark that cell as safe as well.
         """
-        if cell not in self.safes:
-            self.safes.add(cell)
-            for sentence in self.knowledge:
-                sentence.mark_safe(cell)
+        self.safes.add(cell)
+        for sentence in self.knowledge:
+            sentence.mark_safe(cell)
 
     def add_knowledge(self, cell, count):
         """
         Called when the Minesweeper board tells us, for a given
         safe cell, how many neighboring cells have mines in them.
+
+        This function should:
+            1) mark the cell as a move that has been made
+            2) mark the cell as safe
+            3) add a new sentence to the AI's knowledge base
+               based on the value of `cell` and `count`
+            4) mark any additional cells as safe or as mines
+               if it can be concluded based on the AI's knowledge base
+            5) add any new sentences to the AI's knowledge base
+               if they can be inferred from existing knowledge
         """
+        # 1) Mark the cell as a move that has been made
         self.moves_made.add(cell)
+
+        # 2) Mark the cell as safe
         self.mark_safe(cell)
 
+        # 3) Add a new sentence to the AI's knowledge base
         neighbors = set()
         known_mine_count = 0
         for i in range(cell[0] - 1, cell[0] + 2):
             for j in range(cell[1] - 1, cell[1] + 2):
-                if (i, j) == cell:
-                    continue
+                # Ensure the cell is within the board boundaries
                 if 0 <= i < self.height and 0 <= j < self.width:
-                    if (i, j) in self.mines:
+                    neighbor_cell = (i, j)
+                    # Exclude the cell itself
+                    if neighbor_cell == cell:
+                        continue
+                    # Only add undetermined cells to the new sentence
+                    if neighbor_cell in self.mines:
                         known_mine_count += 1
-                    elif (i, j) not in self.safes:
-                        neighbors.add((i, j))
+                    elif neighbor_cell not in self.safes:
+                        neighbors.add(neighbor_cell)
 
         new_sentence = Sentence(neighbors, count - known_mine_count)
+
+        # Adjust count based on already known mines within neighbors
         if new_sentence not in self.knowledge:
             self.knowledge.append(new_sentence)
 
-        # 统一的、更健壮的推理循环
-        while True:
-            changes_made = False
+        # self.knowledge.append(new_sentence)
 
-            # 步骤 A: 标记所有可以直接推断出的安全格和地雷格
-            safes_inferred = set()
-            mines_inferred = set()
+        # 4 & 5) Infer new knowledge (safes, mines, sentences) repeatedly
+        knowledge_changed = True
+        while knowledge_changed:
+            knowledge_changed = False
+
+            # Use known_safes and known_mines to infer new safe spots and mines
+            safes_found = set()
+            mines_found = set()
             for sentence in self.knowledge:
-                safes_inferred.update(sentence.known_safes())
-                mines_inferred.update(sentence.known_mines())
+                safes_found.update(sentence.known_safes())
+                mines_found.update(sentence.known_mines())
 
-            new_safes = safes_inferred - self.safes
-            if new_safes:
-                changes_made = True
-                for safe_cell in new_safes:
-                    self.mark_safe(safe_cell)
+            if safes_found:
+                for safe_cell in safes_found:
+                    if safe_cell not in self.safes:
+                        self.mark_safe(safe_cell)
+                        knowledge_changed = True
 
-            new_mines = mines_inferred - self.mines
-            if new_mines:
-                changes_made = True
-                for mine_cell in new_mines:
-                    self.mark_mine(mine_cell)
+            if mines_found:
+                for mine_cell in mines_found:
+                    if mine_cell not in self.mines:
+                        self.mark_mine(mine_cell)
+                        knowledge_changed = True
 
-            # 清理所有空的句子
+            # Remove empty sentences from knowledge
             self.knowledge = [s for s in self.knowledge if s.cells]
 
-            # 步骤 B: 通过子集规则推断新的句子
+            # Infer new sentences using the subset method
             new_inferences = []
-            knowledge_copy = self.knowledge[:]  # 使用副本进行迭代
-            for s1, s2 in itertools.combinations(knowledge_copy, 2):
-                if s1.cells.issubset(s2.cells):
-                    diff_cells = s2.cells - s1.cells
-                    diff_count = s2.count - s1.count
-                    inferred_sentence = Sentence(diff_cells, diff_count)
-                    if inferred_sentence not in self.knowledge and inferred_sentence not in new_inferences:
-                        new_inferences.append(inferred_sentence)
-                        changes_made = True
+            for s1 in self.knowledge:
+                for s2 in self.knowledge:
+                    if s1.cells == s2.cells:
+                        continue
+                    # If s2 is a subset of s1, create a new sentence
+                    if s2.cells.issubset(s1.cells):
+                        inferred_cells = s1.cells - s2.cells
+                        inferred_count = s1.count - s2.count
+                        inferred_sentence = Sentence(inferred_cells, inferred_count)
+
+                        # Add the new sentence if it's not already in our knowledge
+                        if inferred_sentence not in self.knowledge and inferred_sentence not in new_inferences:
+                            new_inferences.append(inferred_sentence)
+                            knowledge_changed = True
 
             self.knowledge.extend(new_inferences)
-
-            # 如果在一整个循环中没有任何改变，则知识库稳定
-            if not changes_made:
-                break
 
     def make_safe_move(self):
         """
         Returns a safe cell to choose on the Minesweeper board.
+        The move must be known to be safe, and not already a move
+        that has been made.
+
+        This function may use the knowledge in self.mines, self.safes
+        and self.moves_made, but should not modify any of those values.
         """
         safe_moves = self.safes - self.moves_made
         if safe_moves:
-            # 使用 list 转换和 pop(0) 以确保每次返回一个确定的值，便于测试
-            return list(safe_moves)[0]
+            return safe_moves.pop()
         return None
 
     def make_random_move(self):
         """
         Returns a move to make on the Minesweeper board.
+        Should choose randomly among cells that:
+            1) have not already been chosen, and
+            2) are not known to be mines
         """
         possible_moves = []
         for i in range(self.height):
@@ -264,4 +303,5 @@ class MinesweeperAI():
 
         if not possible_moves:
             return None
+
         return random.choice(possible_moves)
